9128b82a136e78ee29e49a536c06144c55cc1199
BrennenMcGill <31702142+BrennenMcGill@users.noreply.github.com>
2020-07-04 12:34:03 -0600

    finished questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const generateFile = require('./readme-template');
+const fs = require('fs');
 
 const promptReadme = () => {
     console.log(`
@@ -11,15 +12,31 @@ const promptReadme = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: name => {
+            if (name) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: descripton => {
+            if (descripton) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
-        type: 'input',
+        type: 'checkbox',
         name: 'installation',
         message: 'What did you this project with? (Check all that apply)',
         choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
@@ -27,38 +44,78 @@ const promptReadme = () => {
       {
         type: 'input',
         name: 'usage',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'How do you use this application?',
+        validate: usage => {
+            if (usage) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
-        type: 'input',
+        type: 'confirm',
         name: 'contributors',
-        message: 'Would you like to feature this project?'
+        message: 'Did you have any contributors on this project?',
+        default: false
       },
       {
         type: 'input',
-        name: 'test',
-        message: 'Would you like to enter another project?'
+        name: 'contributorsTrue',
+        message: 'Please list Who worked on this project:',
+        when: ({ contributors }) => contributors,
+        validate: contributorsTrue => {
+            if (contributorsTrue) {
+              return true;
+            } else {
+              console.log('Please enter a  contributor!');
+              return false;
+            }
+          }
       },
       {
         type: 'list',
         name: 'license',
-        message: 'What kind of license are you using?'
+        message: 'What kind of license are you using?',
+        choices: ['Apache', 'MIT', 'GNU']
       },
       {
         type: 'input',
         name: 'github',
-        message: 'What is your Github username?'
+        message: 'What is your Github username?',
+        validate: github => {
+            if (github) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
         name: 'email',
-        message: 'Please enter your email address'
+        message: 'Please enter your email address',
+        validate: email => {
+            if (email) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       }
     ]);
   };
   
   promptReadme()
-  .then(answers => console.log(answers))
-  .then(projectAnswers => {
-    const pageHTML = generateFile(projectAnswers);
+  .then(readmeData => {
+     const pageHTML = generateFile(readmeData);
+
+     fs.writeFile('./readme.md', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Page created! Check out readme.md in this directory to see it!');
+     });
   });
--- readme-template.js
+++ readme-template.js
@@ -1,21 +1,65 @@
 module.exports = templateData => {
+    const {
+        name,
+        description,
+        installation,
+        usage,
+        contributorsTrue,
+        license,
+        github,
+        email
+    } = templateData;
     console.log(templateData);
   
     return `
-    <!DOCTYPE html>
-    <html lang="en">
-  
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${templateData.name}</h1>
-      <h2><a href="https://github.com/${templateData.languages}">Github</a></h2>
-    </body>
-    </html>
+# ${name}
+
+## Description 
+
+${description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contact](#Contact)
+
+
+## Installation
+
+${installation}
+
+
+## Usage 
+
+${usage}
+ 
+## Credits
+
+${contributorsTrue}
+
+## License
+
+${license}
+
+
+---
+
+🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+
+## Contact
+
+* ${email}
+* ${github}
+
     `;
   };
